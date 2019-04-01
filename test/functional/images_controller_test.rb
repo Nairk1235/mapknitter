45073219f86a0b02f3ed1c6949f7ea0f793744a9
Kaustubh Nair <kaustubh.nair108@gmail.com>
2019-04-02 01:57:25 +0530

    Add tests for maps

---
+++ test/functional/images_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class ImagesControllerTest < ActionController::TestCase
+
+  # called before every single test
+  def setup
+    @map = maps(:saugus)
+  end 
+
+  test "should create image"
+  test "should delete image"
+  test "should display image"
+  test "should update image"
+  test "should import image"
+
+end
